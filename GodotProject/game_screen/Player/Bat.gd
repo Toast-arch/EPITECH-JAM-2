ac6ea455423b0994d7bffef62673e944cd392412
Baptiste GODEAU <baptiste.godeau@epitech.eu>
2021-03-28 04:07:39 +0200

    [MOD] Simple AI - follows player within range

--- GodotProject/game_screen/Player/Bat.gd
+++ GodotProject/game_screen/Player/Bat.gd
@@ -1,17 +1,50 @@
 extends KinematicBody2D
 
 var knockback = Vector2.ZERO
+var velocity = Vector2.ZERO
 
 const EnemyDeathEffect = preload("res://assets/Action RPG Resources/Effects/BatDeathAnimation.tscn")
 
+const ACC = 300
+const MAX_SPEED = 80
+const FRICTION = 200
+
 onready var stats = $Stats
-onready var mainSprite = $Sprite
-onready var shadowSprite = $ShadowSprite
-onready var deathAnim = $BatDeathAnimation
+onready var playerDetection = $PlayerDetection
+onready var sprite = $Sprite
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+var state = CHASE
+
+func look_for_player():
+	if playerDetection.player_locked():
+		state = CHASE
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			look_for_player()
+		WANDER:
+			pass
+		CHASE:
+			var player = playerDetection.player
+			if player != null:
+				var dir = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(dir * MAX_SPEED, ACC * delta)
+			else:
+				state = IDLE
+	
+	sprite.flip_h = velocity.x < 0
+	velocity = move_and_slide(velocity)
 
 func _on_HurtBox_area_entered(area):
 	stats.health -= 1
