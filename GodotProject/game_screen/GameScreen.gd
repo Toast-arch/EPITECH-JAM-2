afac0ff17a7e25239c08964eb4ca4f4328fae830
Baptiste GODEAU <baptiste.godeau@epitech.eu>
2021-03-28 20:03:57 +0200

    [MOD] Added fade in & background music

--- GodotProject/game_screen/GameScreen.gd
+++ GodotProject/game_screen/GameScreen.gd
@@ -3,15 +3,21 @@ extends Node2D
 onready var player = $YSort/Player
 onready var coinUi = $CanvasLayer/CoinUI
 
+var nextScene = "res://menu/GameOver.tscn"
+onready var fadeIn = $CanvasLayer/FadeIn
+
 func _ready():
 	player.connect("game_over", self, "gameOver")
 	coinUi.connect("victory", self, "victory")
-	
+
 func gameOver():
-	get_tree().change_scene("res://menu/GameOver.tscn")
+	fadeIn.show()
+	fadeIn.fade_in()
 
 func victory():
-	get_tree().change_scene("res://menu/VictoryScreen.tscn")
+	fadeIn.show()
+	fadeIn.fade_in()
+	nextScene = "res://menu/VictoryScreen.tscn"
 
-func Victory():
-	pass
+func _on_FadeIn_fade_finished():
+	get_tree().change_scene(nextScene)
--- GodotProject/menu/fade_in/FadeIn.gd
+++ GodotProject/menu/fade_in/FadeIn.gd
@@ -2,8 +2,11 @@ extends ColorRect
 
 signal fade_finished
 
+onready var streamPlayer = $AudioStreamPlayer
+
 func fade_in():
 	$AnimationPlayer.play("fade_in")
+	streamPlayer.playing = true
 
 
 # warning-ignore:unused_argument
